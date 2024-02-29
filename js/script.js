642f1e6a27551d34c1d09bceaae15cfd314b03ff
s4r44d43 <sararez@live.it>
2024-02-29 19:47:16 +0100

    [add] reset

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 
 const bigSq = document.querySelector('.big_square');
+const btnReset = document.querySelector('.reset');
 
 for( let i = 1; i <= 100; i++){
   const insertSq = functionInsertSq (i);
   bigSq.append(insertSq);
 };
 
+
+btnReset.addEventListener('click', function(){
+  reset ();
+});
+
 //FUNCTION
 
 
 // funzione per fare comparire i quadratini colorati e il numero
 function functionInsertSq (numero){
+
   const sq = document.createElement('div');
   sq.className = 'small_square';
   
