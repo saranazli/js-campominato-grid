0c3770eb73c6567538f2d35f903d0324edb85fdc
s4r44d43 <sararez@live.it>
2024-02-29 18:51:01 +0100

    [add] clicked coloured class to small square

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+
 const bigSq = document.querySelector('.big_square');
 
 for( let i = 1; i <= 100; i++){
-  const insertSq = functionInsertSq ();
+  const insertSq = functionInsertSq (i);
   bigSq.append(insertSq);
 };
 
 //FUNCTION
 
-function functionInsertSq (){
+function functionInsertSq (numero){
   const sq = document.createElement('div');
-  sq.className= 'small_square';
+  sq.className = 'small_square';
+  
+
+  sq.addEventListener('click', function(){
+
+    this.innerHTML = numero;
+    this.classList.add('choosed');
+
+  });
 
   return sq;
-}
+};
 
 function reset (){
   bigSq.innerHTML= ' ';
